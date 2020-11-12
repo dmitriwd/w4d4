e0fc1c933864943e07c5c5c7bccea2b1e7cd9896
Filipe Freire <filipefreire@protonmail.com>
2020-11-12 16:48:37 +0100

    Add comments

--- mongoose-intro/index.js
+++ mongoose-intro/index.js
@@ -1,5 +1,11 @@
 const mongoose = require('mongoose');
 
+// Why do we create a Schema?
+// A Schema prevents us from :
+// -> adding fields that shouldn't otherwise exist (because we define the fields ourselves);
+// -> forgetting fields that are required;
+// -> using the wrong type in a field
+
 const bookSchema = new mongoose.Schema({
   title: String,
   author: String,
@@ -35,12 +41,18 @@ const complexBookSchema = new mongoose.Schema({
   },
   genre: {
     type: String,
-    enum: ['Drama', 'Spiritual', 'Romance', 'Manual']
+    enum: ['Drama', 'Spiritual', 'Romance', 'Manual'] // checks the string for one of the values in here
   }
 });
 */
 
 const Book = mongoose.model('Book', bookSchema); // Creates a book collection; Tells Mongoose to use the bookSchema
+
+// Below we use mongoose to perform CRUD Operations:
+// C: Create
+// R: Read
+// U: Update
+// D: Delete
 
 mongoose
   .connect('mongodb://localhost:27017/node-mongoose-introduction', {
@@ -54,17 +66,17 @@ mongoose
   })
   .then(() => {
     console.log('Database Cleared! 🧹');
-    // return Book.create({
-    //   title: 'Filipe"s Mongoose Adventure',
-    //   author: 'Filipe Freire',
-    //   pages: 150,
-    //   releaseDate: new Date(2020, 11, 12),
-    //   isAvailable: true,
-    //   genre: 'Drama'
-    // });
+    return Book.create({
+      title: 'Filipe"s Mongoose Adventure',
+      author: 'Filipe Freire',
+      pages: 150,
+      releaseDate: new Date(2020, 11, 12),
+      isAvailable: true,
+      genre: 'Drama'
+    });
   })
   .then(() => {
-    //console.log('This is our newly created book: ', book);
+    console.log('This is our newly created book: ', book);
     return Book.find({ pages: 1000 });
   })
   .then(foundBooks => {
@@ -80,7 +92,7 @@ mongoose
     return Book.findByIdAndUpdate(
       '5fad40c4fee5ae10716845b4',
       { genre: 'Spiritual' },
-      { new: true }
+      { new: true } // option passed in order to return the updated document
     );
   })
   .then(updatedBook => {
@@ -98,6 +110,10 @@ mongoose
   })
   .then(deletedBook => {
     console.log("The following book isn't available no more! ", deletedBook);
+    return mongoose.disconnect(); // Terminates the connection between mongoose and our database
+  })
+  .then(() => {
+    console.log('Mongoose is now disconnected from our database in MongoDB.');
   })
   .catch(error => {
     console.log('Oh no! Something unexpected has occurred! 😲', error);
